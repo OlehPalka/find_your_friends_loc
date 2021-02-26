ad3ef53a4e6a9605049cf429e99cfbb9667ccef3
OlehPalka <palka280617@gmail.com>
2021-02-26 19:24:45 +0200

    final commit

--- track_location.py
+++ track_location.py
@@ -1,17 +1,24 @@
-import pprint
+"""
+This is module with my first flask project
+"""
 import requests
-import json
 import folium
 from geopy.exc import GeocoderUnavailable
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
+from flask import Flask, render_template, request
+
 
 mapp = folium.Map()
 geolocator = Nominatim(user_agent="sotiy_try.")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+app = Flask(__name__)
 
 
 def twitter_data(access_token, screen_name):
+    """
+    This function gets information about user from Twitter.
+    """
     base_url = "https://api.twitter.com/"
 
     search_headers = {
@@ -28,6 +35,9 @@ def twitter_data(access_token, screen_name):
 
 
 def find_locations(read_file):
+    """
+    This function finds location of each person, you follow.
+    """
     locations = list()
     for i in read_file['users']:
         if i["location"] != "":
@@ -38,6 +48,10 @@ def find_locations(read_file):
 
 
 def find_coordinates(loactions_lst: list):
+    """
+    This function finds coordinates according to the
+    location of people you folow.
+    """
     counter = 0
     while True:
         try:
@@ -54,6 +68,9 @@ def find_coordinates(loactions_lst: list):
 
 
 def adding_points_to_map(locations_with_coordinates: list):
+    """
+    This function adds points of your friends location to the map.
+    """
     for coordinates in locations_with_coordinates:
         folium.Marker(
             location=coordinates[-1], popup=coordinates[0],
@@ -61,12 +78,34 @@ def adding_points_to_map(locations_with_coordinates: list):
 
 
 def main_function(access_token, screen_name):
+    """
+    This id the main function, which returns ready map.
+    """
     read_file = twitter_data(access_token, screen_name)
-    print(read_file)
     loactions_lst = find_locations(read_file)
     locations_with_coordinates = find_coordinates(loactions_lst)
     adding_points_to_map(locations_with_coordinates)
-    mapp.save("lol.html")
+    return mapp
 
 
-print(main_function("AAAAAAAAAAAAAAAAAAAAAO2SMwEAAAAAFIOTZh1di1leanfjtvYE%2FJzmwqg%3D3wqNmOgxKmTbYMeUVsA4Sgxklat8IcI7HYW886jdhhLXH1eaow", "@OlehPalka"))
+@app.route("/")
+def index():
+    """
+    This function generates site.
+    """
+    return render_template("index.html")
+
+
+@app.route("/followers", methods=["POST"])
+def adding_map():
+    """
+    This function add map to the site.
+    """
+    user_name = str(request.form.get("user_name"))
+    api_key = str(request.form.get("API_key"))
+    map_html = main_function(api_key, user_name)
+    return map_html.get_root().render()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
